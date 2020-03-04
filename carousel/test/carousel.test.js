39863fa6161ca287e9706c1afcbdf78c2082a127
arakno <paulobasto@gmail.com>
2020-03-04 12:02:17 +0000

    remove tests

--- carousel/test/carousel.test.js
+++ carousel/test/carousel.test.js
@@ -10,59 +10,14 @@ describe('carousel compoent', () => {
     expect(el.title).to.equal('');
   });
 
-  // it('has a static shadowDom', async () => {
+
+  //  it('should display paragraph if no data', async () => {
   //   const el = (await fixture(html`
   //     <lit-carousel></lit-carousel>
   //   `));
-  //   expect(el).shadowDom.to.equal(`
-  //   <slot name="slides"></slot>
-  // `);
-  // });
-
-
-
-   it('should display paragraph if no data', async () => {
-    const el = (await fixture(html`
-      <lit-carousel></lit-carousel>
-    `));
-    expect(el).lightDom.to.equal(`
-      <p class="visually-hidden">Click, mousewheel or use your keyboard cursor keys</p>
-    `);
-  });
-
-
-
-  // Testing a single unit of logic can be achieved by instantiating the class
-  // it("returns default text", () => {
-  //   const component = new LitCarousel();
-  //   expect(component.getDefaultText()).toEqual("Hello, World!");
-  // });
-
-
-
-  
-
-  // it('has 1 input and 1 label in light-dom', async () => {
-  //   const el = (await fixture(html`
-  //     <lit-carousel .label=${'foo'}></lit-carousel>
-  //   `));
   //   expect(el).lightDom.to.equal(`
-  //     <label slot="label">foo</label>
-  //     <input slot="input">
+  //     <p class="visually-hidden">Click, mousewheel or use your keyboard cursor keys</p>
   //   `);
   // });
 
-  // it('can set/get the input value directly via the custom element', async () => {
-  //   const el = (await fixture(html`
-  //     <lit-carousel .value=${'foo'}></lit-carousel>
-  //   `));
-  //   expect(el.value).to.equal('foo');
-  // });
-
-  // it('can update its label', async () => {
-  //   const el = (await fixture('<lit-carousel label="foo"></lit-carousel>'));
-  //   expect(el.label).to.equal('foo');
-  //   el.label = 'bar';
-  //   expect(el.label).to.equal('bar');
-  // });
 });
